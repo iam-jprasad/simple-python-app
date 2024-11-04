2791772b2460536759b2518082dbed1112d13ea4
iam-pjyoti <jyotiprasad1008@gmail.com>
2024-11-04 23:43:05 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,6 +2,6 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker full ppp001/simple-python-app
+docker pull ppp001/simple-python-app
 # Run the Docker image as a container
 docker run -d -p 5000:5000 ppp001/simple-python-app
