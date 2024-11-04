5e41e3e49b739005b321812231f7183b9a0b1cd0
iam-pjyoti <jyotiprasad1008@gmail.com>
2024-11-04 23:15:57 +0530

    comm

---
+++ scripts/start_container.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker full ppp001/simple-python-app
+# Run the Docker image as a container
+docker run -d -p 5000:5000 ppp001/simple-python-app
